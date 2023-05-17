8ac811c68f53e3335a685dd43ff6e0108255d3fd
imtiaz04 <85178565+imtiaz04@users.noreply.github.com>
2023-05-17 17:14:16 -0400

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 3!from trainWithShubhm",
+        message: "Hello From Batch 3!",
       },
       null,
       2
